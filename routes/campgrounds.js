14593fab71e3dc8a85145a01d8cb19fc6ec18dce
Jacob Rose <jacobdanielrose@gmail.com>
2020-12-03 19:42:28 +0100

    Added git-ignore. linked cloudinary API

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,11 +5,18 @@ const campgrounds = require('../controllers/campgrounds')
 
 const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const multer = require('multer')
+const { response } = require('express')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files)
+        res.send('it worked')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 

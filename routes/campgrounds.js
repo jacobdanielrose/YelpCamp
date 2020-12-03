6ebabaa8f93708f62b85a48141e37da14fa802eb
Jacob Rose <jacobdanielrose@gmail.com>
2020-12-03 20:28:08 +0100

    Set up cloudinary and uploaded some images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,16 +7,13 @@ const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const multer = require('multer')
 const { response } = require('express')
-const upload = multer({ dest: 'uploads/' })
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.files)
-        res.send('it worked')
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 

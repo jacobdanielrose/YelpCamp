6cfbedca9e31900141c0b8d3cdd9cf4521a7724f
Jacob Rose <jacobdanielrose@gmail.com>
2020-12-04 22:07:28 +0100

    leave location same for nwo

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,10 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [12312, 12312], // starting position [lng, lat]
+    center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat([12312, 123123])
+    .setLngLat([-74.5, 40])
     .addTo(map)

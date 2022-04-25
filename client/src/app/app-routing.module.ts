829c9b1ac8efef31014290354dc8dc936fd83a44
Jacob Rose <rose@insevis.de>
2022-04-25 11:54:46 +0200

    added basic routes and set up home path.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CampgroundsComponent } from './campgrounds/campgrounds.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'campgrounds', component: CampgroundsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

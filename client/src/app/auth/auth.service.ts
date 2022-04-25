7197e548091c61c0dbea377ff8a1c248e2427a8d
Jacob Rose <rose@insevis.de>
2022-04-25 16:17:59 +0200

    implementing login functionality

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { AuthData } from './auth-data.model';
+
+const BACKEND_URL = environment.apiUrl + "/user/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  // GETTERS
+  getToken() { }
+
+  getIsAuth() { }
+
+  getUserId() { }
+
+  getAuthStatusListener() { }
+
+  login(email: any, password: any) {
+    const authData: AuthData = { email: email, password: password };
+    console.log(authData);
+    this.router.navigate(['/']).then();
+  }
+
+  logout() {
+    this.router.navigate(['/']).then();
+  }
+
+  private setAuthTimer(duration: number) { }
+
+  autoAuthUser() { }
+
+  // STATIC FUNCTIONS
+  private static saveAuthData(token: string, expirationDate: Date, userId: string) { }
+  private static clearAuthData() { }
+  private static getAuthData() { }
+
 }

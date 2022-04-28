6130489a3b7bef3d6e4ab976cc002ef1353fbc07
Jacob Rose <rose@insevis.de>
2022-04-28 10:02:46 +0200

    map added. need to add functionality

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'createurl'
+  apiUrl: 'createurl',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamFjb2Jyb3NlIiwiYSI6ImNraWFrZzZzNjAzdXMycmswYzExOGRyNmUifQ.x7FRktD6C81DF6LkefyRuQ'
+  }
 };
 
 /*

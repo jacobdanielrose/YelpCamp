b739450397d6436ad0b2f4b2d9bef3218e7778da
Jacob Rose <rose@insevis.de>
2022-04-29 12:16:57 +0200

    custom navbar background change to get rid of transparent background

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'createurl',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamFjb2Jyb3NlIiwiYSI6ImNraWFrZzZzNjAzdXMycmswYzExOGRyNmUifQ.x7FRktD6C81DF6LkefyRuQ'
+  }
 };
